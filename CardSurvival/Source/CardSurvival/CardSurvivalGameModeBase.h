55f95f67f40e8a9fe57039c6f82e024e620ce83c
emqk <jendromij@gmail.com>
2022-05-14 13:15:10 +0200

    Changed the engine from Unity 2021 to UE 5.0.1

---
+++ CardSurvival/Source/CardSurvival/CardSurvivalGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CardSurvivalGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CARDSURVIVAL_API ACardSurvivalGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
