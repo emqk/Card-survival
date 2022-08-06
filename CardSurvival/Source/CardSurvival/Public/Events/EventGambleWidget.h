e9f04ac40fcce5f7ae0f53e06f888739dec335e7
emqk <jendromij@gmail.com>
2022-08-06 23:05:08 +0200

    Event: Random random chance generation

---
+++ CardSurvival/Source/CardSurvival/Public/Events/EventGambleWidget.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "EventGambleWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CARDSURVIVAL_API UEventGambleWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	void Start();
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	void End();
+};
